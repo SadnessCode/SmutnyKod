471d020ea29f37c7b14daee888b1e4ccdf4f862e
Hubert Kamienik <Hubert.Kamienik@ericpol.com>
2016-01-15 15:11:54 +0100

    First Commit , First version of Log Analyser

---
+++ LogAnalysys.hpp
@@ -0,0 +1,35 @@
+/*
+ * LogAnalysys.hpp
+ *
+ *  Created on: Jan 15, 2016
+ *      Author: p637
+ */
+#include <string>
+#ifndef LOGANALYSYS_HPP_
+#define LOGANALYSYS_HPP_
+using namespace std;
+class LogAnalysys
+{
+
+	struct word
+	{
+		bool flag;
+		string  word_char;
+		string  reg;
+	};
+
+	int NumberOfArguments;
+	char ** HandleToArguments;
+	string OutString;
+	string retu;
+	public:
+	LogAnalysys (int Number,char ** a);
+	string  do_it (string & LineOfFile);
+	void set_word (word * name,int number,string & re);
+	void set_reg (word * name,int number,char**argc);
+	void if_match (word * name , int number,string & word);
+
+};
+
+
+#endif /* LOGANALYSYS_HPP_ */
---
+++ LogAsnalysys.cpp
@@ -0,0 +1,80 @@
+/*
+ * LogAsnalysys.cpp
+ *
+ *  Created on: Jan 15, 2016
+ *      Author: p637
+ */
+#include "LogAnalysys.hpp"
+#include <stdlib.h>
+#include <iostream>
+#include <regex.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+LogAnalysys::LogAnalysys (int Number,char ** a): NumberOfArguments(Number),HandleToArguments(a){}
+string LogAnalysys::do_it (string & LineOfFile)
+{
+	word *Flags;
+	Flags= new word [(NumberOfArguments/2)-1];
+	set_reg(Flags,(NumberOfArguments/2)-1, HandleToArguments);
+	if_match(Flags,(NumberOfArguments/2)-1,LineOfFile);
+	set_word(Flags,(NumberOfArguments/2)+1,OutString);
+	OutString+=LineOfFile;
+	retu=OutString;
+
+	delete [] Flags;
+	OutString="";
+	return retu;
+
+
+}
+void LogAnalysys::set_word (word * name,int number,string & re)
+{
+	re=re+'[';
+	for (int i=1;i<number-1;i++)
+	{
+
+		if(name[i-1].flag)
+		{
+			re=re+ name[i-1].word_char;
+		}
+		if(!name[i-1].flag)
+		{
+			re=re+'.';
+		}
+	}
+	re=re+']';
+}
+void LogAnalysys::set_reg (word * name,int number,char**argc)
+{
+
+
+	for (int i=0;i<number;i++)
+	{
+
+		name[i].flag=0;
+		name[i].word_char=string( argc[(i*2+2)]);
+		name[i].reg=string( argc[(i*2)+3]);
+	}
+}
+void LogAnalysys::if_match (word * name , int number,string & word)
+{
+	regex_t regex;
+	int reti;
+	for (int i=0;i<number;i++)
+	{
+		reti = regcomp(&regex, name[i].reg.c_str(), 0);
+		if (reti)
+		{
+			fprintf(stderr, "Could not compile regex\n");
+			exit(1);
+		}
+		reti = regexec(&regex, word.c_str(), 0, NULL, 0);
+		if (!reti) {
+			name[i].flag=1;
+		}
+	}
+}
+
+
+
